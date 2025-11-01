1a13639509d12f36dffa00c762a90741bf216b40
Anne Heimes <ahe@akustik.rwth-aachen.de>
2025-11-01 12:23:31 +0100

    fix: packaging not found

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,7 +1,6 @@
 import re
 import sys
 
-from packaging import version
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 module_name = '{{ cookiecutter.project_slug}}'
@@ -25,13 +24,6 @@ MODULE_REGEX = '3.[0-9]+'
 minimum_python_version = '{{ cookiecutter.minimum_python_version }}'
 if not re.match(MODULE_REGEX, minimum_python_version):
     print(f'ERROR: The project slug ({minimum_python_version}) is not a valid Python version number. Please use python 3.x')
-
-    #Exit to cancel project
-    sys.exit(1)
-
-
-if version.parse(default_python_version) < version.parse(minimum_python_version):
-    print(f'ERROR: The default python version ({default_python_version}) is not greater or equal than the minimum python version ({minimum_python_version}). Please use a default python version greater or equal than the minimum python version')
 
     #Exit to cancel project
     sys.exit(1)
